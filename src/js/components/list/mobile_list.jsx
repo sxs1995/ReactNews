04ddb33ce47b27c399207ee33371b385b3047a34
Sxians <l>
2018-10-18 09:27:08 +0800

    移动端布局

---
+++ src/js/components/list/mobile_list.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Card, Row, Col } from "antd";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+export default class PCNewsBlock extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      news: ""
+    };
+  }
+
+  componentDidMount() {
+    var myFetchOptions = {
+      method: "GET"
+    };
+    fetch(
+      "http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=" +
+        this.props.type +
+        "&count=" +
+        this.props.count,
+      myFetchOptions
+    )
+      .then(response => response.json())
+      .then(json => this.setState({ news: json }));
+  }
+  render() {
+    const { news } = this.state;
+    const newsList = news.length
+      ? news.map((newsItem, index) => (
+          <section
+            key={index}
+            className="m_article list-item special_section clearfix"
+          >
+            <Router>
+              <Link to={`detail/${newsItem.uniquekey}`}>
+                <div className="m_article_img">
+                  <img
+                    src={newsItem.thumbnail_pic_s}
+                    alt="{newsItem.title}"
+                  />
+                </div>
+                <div className="m_article_info">
+                    <div className="m_article_title">
+                        <span>{newsItem.title}</span>
+                    </div>
+                    <div className="m_article_desc clearfix">
+                        <div className="m_article_desc_l">
+                            <span className="m_article_channel">{newsItem.realtype}</span>
+                            <span className="m_article_time">{newsItem.date}</span>
+                        </div>
+                    </div>
+                </div>
+              </Link>
+            </Router>
+          </section>
+        ))
+      : "";
+    return (
+      <div className="topNewsList">
+        <Row>
+          <Col span={24}>
+            {newsList}
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
--- src/js/components/mobile_index.jsx
+++ src/js/components/mobile_index.jsx
@@ -1,20 +1,58 @@
 import React from "react";
 import MobileHeader from "./header/mobile_header";
 import MobileFooter from "./footer/mobile_footer";
-import { Tabs } from "antd";
+import { Tabs, Carousel } from "antd";
+import MobileList from "./list/mobile_list";
+import carousel_1 from "../../images/carousel_1.jpg";
+import carousel_2 from "../../images/carousel_2.jpg";
+import carousel_3 from "../../images/carousel_3.jpg";
+import carousel_4 from "../../images/carousel_4.jpg";
 const TabPane = Tabs.TabPane;
+
 export default class MobileIndex extends React.Component {
   render() {
+    const setting = {
+      dots: true,
+      autoplay: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1
+    };
     return (
       <div>
         <MobileHeader />
         <Tabs>
-          <TabPane tab="头条" key="1" >头条</TabPane>
-          <TabPane tab="社会" key="2" >社会</TabPane>
-          <TabPane tab="国内" key="3" >国内</TabPane>
-          <TabPane tab="国际" key="4" >国际</TabPane>
-          <TabPane tab="娱乐" key="5" >娱乐</TabPane>
-          <TabPane tab="头条" key="6" >头条</TabPane>
+          <TabPane tab="头条" key="1">
+            <div className="carousel">
+              <Carousel effect="fade" {...setting}>
+                <div>
+                  <img src={carousel_1} alt="" />
+                </div>
+                <div>
+                  <img src={carousel_2} alt="" />
+                </div>
+                <div>
+                  <img src={carousel_3} alt="" />
+                </div>
+                <div>
+                  <img src={carousel_4} alt="" />
+                </div>
+              </Carousel>
+            </div>
+            <MobileList count={20} type="top" />
+          </TabPane>
+          <TabPane tab="社会" key="2">
+            <MobileList count={20} type="shehui" />
+          </TabPane>
+          <TabPane tab="国内" key="3">
+            <MobileList count={20} type="guonei" />
+          </TabPane>
+          <TabPane tab="国际" key="4">
+            <MobileList count={20} type="guoji" />
+          </TabPane>
+          <TabPane tab="娱乐" key="5">
+            <MobileList count={20} type="yule" />
+          </TabPane>
         </Tabs>
         <MobileFooter />
       </div>
